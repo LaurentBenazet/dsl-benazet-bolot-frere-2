e5d407c87336db8b1974bb8adf3875a569db3915
Laurent Benazet <laurent-jerome.benazet-lacarre@etu.univ-cotedazur.fr>
2020-02-12 10:58:57 +0100

    Added new way to define instruments in the scenarios

--- ArduinoML-kernel/embeddeds/dsl-groovy/src/main/resources/scripts/BillieJean.groovy
+++ ArduinoML-kernel/embeddeds/dsl-groovy/src/main/resources/scripts/BillieJean.groovy
@@ -2,12 +2,16 @@ package scripts
 
 track "Billie Jean"
 
+instrument "ch" type "closed hithat"
+instrument "sd" type "snare drum"
+instrument "bd" type "bass drum"
+
 section "Main"
 tempo 118
 beats 4
 
-instr "ch" 	    bar "e e e e e e e e" bar "e e e e e e e e"
-instr "sd" 	    bar "_ q _ q" bar "_ q _ q"
-instr "bd"   	bar "q _ q _" bar "q _ q _"
+instr "ch" bar "e e e e e e e e" bar "e e e e e e e e"
+instr "sd" bar "_ q _ q" bar "_ q _ q"
+instr "bd" bar "q _ q _" bar "q _ q _"
 
 play "Main"
--- ArduinoML-kernel/embeddeds/dsl-groovy/src/main/resources/scripts/JohnnyBGoode.groovy
+++ ArduinoML-kernel/embeddeds/dsl-groovy/src/main/resources/scripts/JohnnyBGoode.groovy
@@ -2,12 +2,16 @@ package scripts
 
 track "Johnny B Goode"
 
+instrument "rc" type "ride cymbal"
+instrument "sd" type "snare drum"
+instrument "bd" type "bass drum"
+
 section "Main"
 tempo 170
 beats 4
 
-instr "rc" 	    bar "_ _ _ _" bar "_ _ _ _" bar "e e e e e e e e" bar "e e e e e e e e"
-instr "sd" 	    bar "q _ _ _" bar "_ _ _ q" bar "_ q _ q" bar "_ q _ q"
-instr "bd"   	bar "_ _ _ _" bar "_ _ _ _" bar "q _ q _" bar "q _ q _"
+instr "rc" bar "_ _ _ _" bar "_ _ _ _" bar "e e e e e e e e" bar "e e e e e e e e"
+instr "sd" bar "q _ _ _" bar "_ _ _ q" bar "_ q _ q" bar "_ q _ q"
+instr "bd" bar "_ _ _ _" bar "_ _ _ _" bar "q _ q _" bar "q _ q _"
 
 play "Main"
