88cc334c6363417fb1e1aa4c81059ef0f307a88d
Laurent Benazet <laurent-jerome.benazet-lacarre@etu.univ-cotedazur.fr>
2020-02-12 09:54:12 +0100

    Added 4 first bars for Johnny B Goode

---
+++ ArduinoML-kernel/embeddeds/dsl-groovy/src/main/resources/scripts/JohnnyBGoode.groovy
@@ -0,0 +1,13 @@
+package scripts
+
+track "Johnny B Goode"
+
+section "Main"
+tempo 170
+beats 4
+
+instr "rc" 	    bar "_ _ _ _" bar "_ _ _ _" bar "e e e e e e e e" bar "e e e e e e e e"
+instr "sd" 	    bar "q _ _ _" bar "_ _ _ q" bar "_ q _ q" bar "_ q _ q"
+instr "bd"   	bar "_ _ _ _" bar "_ _ _ _" bar "q _ q _" bar "q _ q _"
+
+play "Main"
