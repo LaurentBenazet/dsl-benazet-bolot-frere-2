df31f33653cc3816a013d1955294eb2eb8e31b30
Laurent Benazet <laurent-jerome.benazet-lacarre@etu.univ-cotedazur.fr>
2020-02-12 09:32:33 +0100

    Two first bars created for Billie Jean

---
+++ ArduinoML-kernel/embeddeds/dsl-groovy/src/main/resources/scripts/BillieJean.groovy
@@ -0,0 +1,13 @@
+package scripts
+
+track "Billie Jean"
+
+section "Main"
+tempo 118
+beats 4
+
+instr "ch" 	    bar "e e e e e e e e" bar "e e e e e e e e"
+instr "sd" 	    bar "_ q _ q" bar "_ q _ q"
+instr "bd"   	bar "q _ q _" bar "q _ q _"
+
+play "Main"
