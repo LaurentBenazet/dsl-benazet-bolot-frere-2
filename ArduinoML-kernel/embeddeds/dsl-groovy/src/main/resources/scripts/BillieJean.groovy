5ef79ee9aac7ab835d431bb4c87455d44edcbb7e
LaurentBenazet <benazetlaurent@gmail.com>
2020-03-05 20:42:50 +0100

    Completed billie jean

--- ArduinoML-kernel/embeddeds/dsl-groovy/src/main/resources/scripts/BillieJean.groovy
+++ ArduinoML-kernel/embeddeds/dsl-groovy/src/main/resources/scripts/BillieJean.groovy
@@ -10,8 +10,8 @@ section "Main"
 tempo 118
 beats 4
 
-instr "ch" bar "e e e e e e e e" bar "e e e e e e e e"
-instr "sd" bar "_ q _ q" bar "_ q _ q"
-instr "bd" bar "q _ q _" bar "q _ q _"
+instr "ch" bar "e e e e e e e e" repeat 34
+instr "sd" bar "_ q _ q" repeat 34
+instr "bd" bar "q _ q _" repeat 34
 
 play "Main"
