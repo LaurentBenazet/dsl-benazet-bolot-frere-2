5a1b10f40b32658c0185c9866467387125199e14
LaurentBenazet <benazetlaurent@gmail.com>
2020-03-06 21:07:21 +0100

    Finished billie jean with piano

--- RhythmML/embeddeds/dsl-groovy/src/main/resources/scripts/BillieJeanWithPiano.groovy
+++ RhythmML/embeddeds/dsl-groovy/src/main/resources/scripts/BillieJeanWithPiano.groovy
@@ -11,10 +11,10 @@ instrument "piano" type "piano"
 section "No piano"
 beats 4
 
-instr "ch" bar "e e e e e e e e" repeat 14
-instr "sd" bar "_ q _ q" repeat 14
-instr "bd" bar "q _ q _" repeat 14
-instr "piano" bar "_ _ _ _" repeat 14
+instr "ch" bar "e e e e e e e e"
+instr "sd" bar "_ q _ q"
+instr "bd" bar "q _ q _"
+instr "piano" bar "_ _ _ _"
 
 section "Main"
 beats 4
@@ -22,9 +22,9 @@ beats 4
 instr "ch" bar "e e e e e e e e" repeat 8
 instr "sd" bar "_ q _ q" repeat 8
 instr "bd" bar "q _ q _" repeat 8
-instr "piano" bar "_ _ _ _" bar "- e(D) e(D) e(D) e(D) e(C) e(D) e(C)" bar "- q(D) s(C) s(D) e(C) e(D) e(D) -"
-instr "piano" bar "- e(D) e(D) e(D) e(D) e(C) e(D) e(C)" bar "- q(D) q(C) e(D) e(C#) e(E)" bar "q(E) _ _ s(E) s(F) e(D)"
-instr "piano" bar "q(D) s(E) s(F) e(D) q(D) s(E) s(F) e(D)" bar "q(D) e(D) - _ _"
+instr "piano" bar "_ _ _ _" bar "_ e(C#) e(C#) e(C#) e(B) e(A) e(B)" bar "e(B) q(C#) s(B) s(A) e(B) e(A) e(C#) -"
+instr "piano" bar "- e(C#) e(C#) e(C#) e(C#) e(B) e(A) e(B)" bar "e(B) e(A) e(C#) q(B) e(A) e(G#-1) e(F#-1)"
+instr "piano" bar "q(F#-1) _ _ s(F#) s(G#) e(F#)" bar "e(F#) - s(F#) s(G#) e(F#) e(F#) - s(F#) s(G#) e(F#)" bar "e(F#) - _ _ _"
 
 section "Transition"
 beats 4
@@ -32,8 +32,8 @@ beats 4
 instr "ch" bar "e e e e e e e e" repeat 4
 instr "sd" bar "_ q _ q" repeat 4
 instr "bd" bar "q _ q _" repeat 4
-instr "piano" bar "- e(E) e(D) q(C) e(D) e(C#) e(E)" bar "q(E) e(E) _ - s(E) s(F) e(D)"
-instr "piano" bar "q(D) s(E) s(F) e(D) q(D) s(E) s(F) e(D)" bar "q(D) e(D) - _ _"
+instr "piano" bar "- e(F#-1) e(A) q(B) e(A) e(G#-1) e(F#-1)" bar "q(F#-1) _ _ s(F#) s(G#) e(F#)"
+instr "piano" bar "e(F#) - s(F#) s(G#) e(F#) e(F#) - s(F#) s(G#) e(F#)" bar "q(F#) _ _ _"
 
 
 play "No piano", "Main", "Transition", "Main"
